d78df6459c64512dde56e6b4425cfaccf887dd4e
Jason Smith <jasonlsmith52@gmail.com>
2017-08-04 10:30:39 -0500

    added order class

---
+++ src/Bangazon/Models/Order.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Bangazon
+{
+    public class Order
+    {
+        public int id {get; set;}
+        public Customer customer {get; set;}
+        public PaymentType payment {get; set;}
+        public DateTime dateCreated {get; set;}
+    }
+}
