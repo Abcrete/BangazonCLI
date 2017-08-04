38b19d32717bc42ec7b34a8d7d3eb88eec053000
Tamela Lerma <tlerma83@gmail.com>
2017-08-04 10:52:14 -0500

    testing Customer

--- test/Bangazon.Tests/Bangazon_CustomerManagerShould.cs
+++ test/Bangazon.Tests/Bangazon_CustomerManagerShould.cs
@@ -1,14 +1,28 @@
 using System;
 using Xunit;
+using Bangazon.Models;
+using Bangazon.Managers;
 
 namespace Bangazon.Tests
 {
     public class CustomerManagerShould
     {
+
+        // private readonly RegisterCustomer _register;
+
+        // public CustomerManagerShould()
+        // {
+        //     _register = new RegisterCustomer();
+        // }
+
+
         [Fact]
         public void AddNewCustomer()
         {
-
+            var custManager = new CustomerManager();
+            var cust = new Customer("Sarah", "Jones", "563756", "Nash", "TN", "37128", "615-787-8898");
+            bool result = custManager.AddCustomer(cust);
+            Assert.True(result);
         }
 
         [Fact]
