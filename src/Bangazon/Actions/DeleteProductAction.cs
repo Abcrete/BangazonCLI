361664fea0a219e293b0abeb10bedff9dbfbf0cc
Tamela Lerma <tlerma83@gmail.com>
2017-08-08 15:45:13 -0500

    Added delete product action file

---
+++ src/Bangazon/Actions/DeleteProductAction.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using Bangazon.Models;
+using Bangazon.Managers;
+
+namespace Bangazon.Actions
+{
+    public class DeleteProductAction
+    {
+        
+    }
+}
